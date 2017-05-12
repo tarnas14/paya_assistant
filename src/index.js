9c44a482a47e6d7c8242269bc5ce9dd3a6b4e4ef
Krzysztof Jendrzyca <hapus1@gmail.com>
2017-05-12 17:52:48 +0200

    Added basic react-router setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -9,9 +14,22 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App';
 import './index.css';
 
+const AppWrapper = () => (
+  <div>
+    <Route path="/" component={App}/>
+    <Route path="/tiphistory" component={App}/>
+    <Route path="/stats" component={App}/>
+    <Route path="/myaccount" component={App}/>
+  </div>
+)
+
 ReactDOM.render(
   <MuiThemeProvider>
-    <App />
+    <Router>
+      <div>
+        <Route path="/" component={AppWrapper} />
+      </div>
+    </Router>
   </MuiThemeProvider>,
   document.getElementById('root')
 );
