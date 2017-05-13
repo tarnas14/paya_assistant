3df8a4a03c2ee83fd985ff6fa1f373469d990002
Krzysztof Jendrzyca <hapus1@gmail.com>
2017-05-13 08:08:04 +0200

    Change isDev implementation to use custom env variable instead of NODE_ENV that is not overridable

--- src/components/RedirectToLoginPage.js
+++ src/components/RedirectToLoginPage.js
@@ -3,16 +3,16 @@ import Content from '../components/Content'
 import logo from '../images/logo.png'
 import RaisedButton from 'material-ui/RaisedButton'
 
-const isDev = () => process.env.NODE_ENV === 'development'
+const isDev = () => process.env.REACT_APP_IS_DEV === 'true'
 
 export default () => {
   const redirectUri = `${document.location.origin}/login?afterLoginGoTo=${document.location.pathname}`
-  const backendRedirectString = `${process.env.REACT_APP_API_LOGIN}?redirectUri=${encodeURIComponent(redirectUri)}`
+  const backendRedirectString = `${process.env.REACT_APP_API_LOGIN}?redirectUrl=${encodeURIComponent(redirectUri)}`
 
   const authroute = `${document.location.origin}/login?afterLoginGoTo=${encodeURIComponent(document.location.pathname)}&token=somethingSomething`
 
   return <Content>
-    <img src={logo} style={{width: '100%'}} alt='tiper <3 payments.' />
+    <img src={logo} style={{width: '100%', maxWidth: '300px'}} alt='tiper <3 payments.' />
     <div style={{marginTop: '20px'}}><RaisedButton label='Log in' primary={true} href={isDev() ? authroute : backendRedirectString} /></div>
   </Content>
 }
