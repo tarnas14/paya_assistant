f1b6f2fead76c8b50dd4dc769fe4b0315b8ecfd3
Krzysztof Jendrzyca <hapus1@gmail.com>
2017-05-13 07:48:53 +0200

    Fix logo size on mobile devices

--- src/components/RedirectToLoginPage.js
+++ src/components/RedirectToLoginPage.js
@@ -12,7 +12,7 @@ export default () => {
   const authroute = `${document.location.origin}/login?afterLoginGoTo=${encodeURIComponent(document.location.pathname)}&token=somethingSomething`
 
   return <Content>
-    <img src={logo} alt='tiper <3 payments.' />
+    <img src={logo} style={{width: '100%'}} alt='tiper <3 payments.' />
     <div style={{marginTop: '20px'}}><RaisedButton label='Log in' primary={true} href={isDev() ? authroute : backendRedirectString} /></div>
   </Content>
 }
