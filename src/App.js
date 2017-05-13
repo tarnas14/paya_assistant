132a15c36dc6ec6a131b7e9842f6eba99e4c4c10
tarnas14 <tarnas14@gmail.com>
2017-05-13 03:23:20 +0200

    added icons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 import User from './components/User'
 
+import RemoveRedEye from 'material-ui/svg-icons/image/remove-red-eye'
+import Assessment from 'material-ui/svg-icons/action/assessment'
+import AccountBox from 'material-ui/svg-icons/action/account-box'
+import EuroSymbol from 'material-ui/svg-icons/editor/monetization-on'
+import Time from 'material-ui/svg-icons/device/access-time'
+import Divider from 'material-ui/Divider'
+import FontIcon from 'material-ui/FontIcon'
+
 import {Link} from 'react-router-dom'
 
 import './App.css'
@@ -40,31 +48,44 @@ class App extends Component {
         >
           <User user={this.props.user}/>
           <MenuItem
+            style={{textAlign: 'left'}}
+            leftIcon={<RemoveRedEye />}
             containerElement={<Link to="/" />}
             onTouchTap={this.handleMenuClose}
             primaryText="My QR Code"
           />
           <MenuItem
+            leftIcon={<EuroSymbol/>}
+            style={{textAlign: 'left'}}
             containerElement={<Link to="/tip" />}
             onTouchTap={this.handleMenuClose}
             primaryText="Tip"
           />
           <MenuItem
+            style={{textAlign: 'left'}}
+            leftIcon={<Time/>}
             containerElement={<Link to="/tiphistory" />}
             primaryText="History"
             onTouchTap={this.handleMenuClose}
           />
           <MenuItem
+            style={{textAlign: 'left'}}
+            leftIcon={<Assessment />}
             containerElement={<Link to="/stats" />}
             primaryText="Stats"
             onTouchTap={this.handleMenuClose}
           />
           <MenuItem
+            style={{textAlign: 'left'}}
+            leftIcon={<AccountBox />}
             containerElement={<Link to="/myaccount" />}
             primaryText="MyAccount"
             onTouchTap={this.handleMenuClose}
           />
+          <Divider/>
           <MenuItem
+            style={{textAlign: 'left'}}
+            leftIcon={<FontIcon/>}
             containerElement={<Link to="/logout" />}
             primaryText="Logout"
             onTouchTap={this.handleMenuClose}
