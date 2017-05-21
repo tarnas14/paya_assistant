2826f03419a67e0689dee9cf4d5b7a772c422bec
tarnas14 <tarnas14@gmail.com>
2017-05-21 02:10:56 +0200

    moved lines to a languagepack

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -16,26 +16,73 @@ const wait = async (delay = 100) => new Promise(resolve => {
   window.setTimeout(() => resolve(), delay)
 })
 
+const languagePacks = [
+  {
+    predicate: () => true,
+    commands: {
+    
+    },
+    lines: {
+      availableCommands: () => 'Dostępne komendy to:',
+      and: () => 'oraz',
+      tryAgain: () => 'Spróbuj ponownie',
+      greeting: name => `Witaj, ${name}`,
+      howCanIHelpYou: () => 'W czym mogę Ci pomóc?',
+      couldNotCatchThat: () => 'Nie dosłyszałam. Sprawdź czy masz włączony mikrofon.',
+      unknownCommand: () => 'Nie rozpoznałam komendy.',
+      howElseCanIHelpYou: () => 'W czym jeszcze mogę Ci pomóc?',
+      ifYouReallyMustKnow: () => 'Jeśli już koniecznie chcesz wiedzieć.',
+      noProblem: () => 'Nie ma za co ;)',
+      yourPendingPayments: () => 'Twoje płatności.',
+      youHavePayments: pending => `Masz ${pending}. Chcesz się nimi teraz zająć?`,
+      paymentDescription: payment => `${payment.name}. ${payment.amount}zł`,
+      paying: name => `Opłacam ${name}`,
+      oneMoment: () => 'Chwilka',
+      considerItDone: () => 'Załatwione',
+      skipping: name => `Pomijam ${name}`,
+      thatWasTheLastOne: () => 'To była ostatnia płatność na liście',
+      paid: count => `Uregulowane płatności: ${count}`,
+      skipped: count => `Płatności pominięte: ${count}`,
+      thatsAll: name => `To by było na tyle, ${name}`
+    }
+  }
+]
+
 const nullStarted = {started: () => {}}
 
-const speech = async (settings, speaking = () => {}, listening = () => {}, notListening = () => {}) => {
+const speech = async (settings, setPack, speaking = () => {}, listening = () => {}, notListening = () => {}, debug = () => {}) => {
   const synthesis = window.speechSynthesis
 
   const voice = await new Promise(resolve => {
     synthesis.onvoiceschanged = () => resolve(
-      synthesis.getVoices().find(v => v.lang === settings.lang)
-    )
+      synthesis.getVoices().map(v => {
+        debug(`/${v.lang.trim()}/`)
+        return v
+      }).find(v => settings.langs.includes(v.lang)))
   })
+
+  const languagePack = languagePacks.find(pack => pack.predicate(voice.lang))
+  setPack(languagePack)
+  const {lines, commands} = languagePack
 
   const utt = new SpeechSynthesisUtterance()
   utt.pitch = settings.pitch
   utt.rate = settings.rate
   utt.voice = voice
+  debug('///')
+  debug(JSON.stringify(utt.voice.lang))
+  debug(JSON.stringify(utt.voice.default))
+  debug(JSON.stringify(utt.voice.localService))
+  debug(JSON.stringify(utt.voice.name))
+  debug(JSON.stringify(utt.voice.voiceURI))
+  debug('///')
   utt.onboundary = () => console.log('boundary')
   utt.onmark = () => console.log('mark')
   utt.onpause = () => console.log('pause')
 
   const say = async (text, c) => {
+    debug(`volume ${utt.volume}`)
+    debug(`saying ${text}`)
     const callbacks = {
       started: () => speaking(text) || console.log('speaking', text),
       finished: () => {},//console.log('stopped speaking', text),
@@ -51,19 +98,26 @@ const speech = async (settings, speaking = () => {}, listening = () => {}, notLi
 
     utt.text = text
     return new Promise((resolve, reject) => {
-      utt.onstart = callbacks.started
+      utt.onstart = () => {
+        debug(`start of ${text}`)
+        callbacks.started()
+      }
 
-      utt.onerror = () => {
+      utt.onerror = (e) => {
+        debug(JSON.stringify(e))
         callbacks.onerror()
         reject()
       }
 
       utt.onend = () => {
+        debug(`end of ${text}`)
         callbacks.finished()
         resolve()
       }
-
+      
+      debug('<==>')
       synthesis.speak(utt)
+      debug('<==>')
     })
   }
 
@@ -97,7 +151,7 @@ const speech = async (settings, speaking = () => {}, listening = () => {}, notLi
     const recognition = new speechRecognition()
     // doesnt work for some reason
     addCommandsGrammar(recognition, commands)
-    recognition.lang = settings.lang
+    recognition.lang = voice.lang
     recognition.interimResults = false
     recognition.maxAlternatives = 1
 
@@ -110,18 +164,18 @@ const speech = async (settings, speaking = () => {}, listening = () => {}, notLi
       if (bounceCounter % 3 === 0) {
         return
       }
-      await say('Dostępne komendy to.')
+      await say(lines.availableCommands())
       const availableCommands = commands.map(c => c.waitFor)
       for(let i = 0; i < availableCommands.length; ++i) {
         const availableCommand = availableCommands[i]
         await say(availableCommand)
         if (i + 2 === availableCommands.length) {
-          await say('oraz') 
+          await say(lines.and()) 
         } else {
           await wait(200)
         }
       }
-      await say('Spróbuj jeszcze raz.')
+      await say(lines.tryAgain())
     }
 
     return new Promise((resolve, reject) => {
@@ -132,7 +186,7 @@ const speech = async (settings, speaking = () => {}, listening = () => {}, notLi
       recognition.onerror = async (e) => {
         if (e.error === 'no-speech') {
           bounce()
-          await say('Nie dosłyszałam. Sprawdź czy masz włączony mikrofon.')
+          await say(lines.couldNotCatchThat())
           recognition.stop()
           recognition.start()
           return
@@ -150,7 +204,7 @@ const speech = async (settings, speaking = () => {}, listening = () => {}, notLi
         const command = commands.find(c => c.waitFor.toLowerCase() === hit.toLowerCase())
         if (!command) {
           bounce()
-          await say(`Nie rozpoznałam komendy.`)
+          await say(lines.unknownCommand())
           await listCommands()
 
           recognition.stop()
@@ -175,6 +229,7 @@ const speech = async (settings, speaking = () => {}, listening = () => {}, notLi
 
 const settings = {
   lang: 'pl-PL',
+  langs: ['pl-PL', 'pl_PL', 'en_GB'],
   pitch: 1,
   rate: 1
 }
@@ -184,11 +239,15 @@ export default class extends Component {
   constructor () {
     super()
     this.state = {
+      debug: [],
+      langPack: {lines: {}, commands: {}},
       speech: speech(
         settings,
+        langPack => console.log(langPack) || this.setState({langPack}),
         text => this.setState({speaking: text}),
         () => this.setState({listening: true}),
         () => this.setState({listening: false}),
+        // a => this.setState(s => ({debug: [a, ...s.debug]}))
       )
     }
   }
@@ -201,24 +260,30 @@ export default class extends Component {
 
   async greet (user) {
     const s = await this.state.speech
-    await s.say(`Witaj, ${user.name}`, {finished: () => console.log('stopped speaking')})
-    await s.say('W czym moge Ci pomóc?', {finished: () => console.log('stopped speaking')}) 
+    const {langPack: {lines}} = this.state
+    await s.say(lines.greeting(user.name))
+    await s.say(lines.howCanIHelpYou()) 
   }
 
   async next () {
     const s = await this.state.speech
-    const command = await s.waitForCommand([
-      { waitFor: 'płatności', command: this.payments.bind(this) },
-      { waitFor: 'jaki jest sens życia', command: this.meaningOfLife.bind(this)}
-    ])
-    await command()
+    const {langPack: {lines}} = this.state
+    while(true) {
+      const command = await s.waitForCommand([
+        { waitFor: 'płatności', command: this.payments.bind(this) },
+        { waitFor: 'jaki jest sens życia', command: this.meaningOfLife.bind(this)}
+      ])
+      await command()
+      await s.say(lines.howElseCanIHelpYou())
+    }
   }
 
   async meaningOfLife () {
     const s = await this.state.speech
-    await s.say('Jeśli już koniecznie chcesz wiedzieć.')
+    const {langPack: {lines}} = this.state
+    await s.say(lines.ifYouReallyMustKnow())
     await s.say('42')
-    await s.say('Nie ma za co ;)')
+    await s.say(lines.noProblem())
   }
 
   listening = l => this.setState({opacity: l ? 1 : 0.7})
@@ -231,6 +296,7 @@ export default class extends Component {
   success = () => this.setState({showProgressIndicator: false, success: true})
 
   async payments () {
+    const {langPack: {lines}} = this.state
     const getPaymentsString = number => {
       const map = ['jedną zaległą płatność', 'dwie zaległe płatności', 'trzy zaległe płatności',
         'cztery zaległe płatności', 'pięć zaległych płatności', 'sześć zaległych płatności', 'siedem zaległych płatności', 'osiem zaległych płatności', 'dziewięć zaległych płatności', 'dziesięć zaległych płatności']
@@ -241,25 +307,25 @@ export default class extends Component {
     const s = await this.state.speech
     const pendingPayments = await this.state.pendingPayments
 
-    await s.say('Twoje płatności.')
-    await s.say(`Masz ${getPaymentsString(pendingPayments.length)}. Chcesz się nimi teraz zająć?`)
+    await s.say(lines.yourPendingPayments())
+    await s.say(lines.youHavePayments(getPaymentsString(pendingPayments.length)))
     const listPayments = async (payments) => {
       let skipped = 0
       for(let i = 0; i < payments.length; ++i) {
         const payment = payments[i]
         this.setPayment(payment)
-        await s.say(`${payment.name}. ${payment.amount}zł`)
+        await s.say(lines.paymentDescription(payment))
         const command = await s.waitForCommand([
           {
             waitFor: 'zapłać',
             command: async () => {
               this.progress()
-              await s.say(`Opłacam ${payment.name}`)
+              await s.say(lines.paying(payment.name))
               await wait(800)
-              await s.say('Chwilka')
+              await s.say(lines.oneMoment())
               await wait(800)
               this.success()
-              await s.say('Załatwione')
+              await s.say(lines.considerItDone())
               await wait(800)
               this.clearPayment()
             }
@@ -268,17 +334,17 @@ export default class extends Component {
             waitFor: 'dalej',
             command: async () => {
               skipped++
-              await s.say(`Pomijam ${payment.name}`, nullStarted)
+              await s.say(lines.skipping(payment.name), nullStarted)
               this.clearPayment()
             }
           },
         ])
         await command()
       }
-      await s.say('To była ostatnia płatność na liście')
-      await s.say(`Uregulowane płatności: ${payments.length - skipped}`)
-      await s.say(`Płatności pominięte: ${skipped}`)
-      await s.say(`To by było na tyle, ${this.props.user.name}`)
+      await s.say(lines.thatWasTheLastOne())
+      await s.say(lines.paid(payments.length - skipped))
+      await s.say(lines.skipped(skipped))
+      await s.say(lines.thatsAll(this.props.user.name))
     }
 
     const command = await s.waitForCommand([{waitFor: 'tak', command: listPayments.bind(null, pendingPayments)}])
@@ -288,7 +354,7 @@ export default class extends Component {
   render () {
     const hal = process.env.REACT_APP_HAL
     const assistent = hal ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/220px-HAL9000.svg.png' : assistentImage
-    const {opacity, listening, currentPayment, showProgressIndicator, success, speaking} = this.state
+    const {debug, opacity, listening, currentPayment, showProgressIndicator, success, speaking} = this.state
     return <div className='Home' style={{backgroundImage: `url(${assistent})`}}>
       <Card>
         {!currentPayment && speaking && <CardTitle title={speaking}/>}
@@ -300,6 +366,7 @@ export default class extends Component {
         {currentPayment && success && <CardText><span style={{color: cyan300, fontSize: '2em'}}>Załatwione!</span></CardText>}
       </Card>
       {listening && <LinearProgress mode="indeterminate"/>}
+      {debug && <div>{debug.map(d => <p>{d}</p>)}</div>}
     </div>
   }
 }
