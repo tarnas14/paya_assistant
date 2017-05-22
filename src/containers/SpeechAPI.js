a7fa6b08ede758575be70b3d2acfb3a58e1a33f7
tarnas14 <tarnas14@gmail.com>
2017-05-22 22:35:53 +0200

    added information about supported browsers

---
+++ src/containers/SpeechAPI.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {CardText, CardTitle, CardHeader, Card} from 'material-ui/Card'
+import Content from '../components/Content'
+
+const synthesis = Boolean(window.speechSynthesis)
+const recognition = Boolean(window.SpeechRecognition || window.webkitSpeechRecognition)
+
+export default ({children}) =>{
+  if (!(synthesis && recognition)) {
+    return <Content><Card>
+      <CardText>Do korzystania z asystenta głosowego PAYA potrzebujesz przeglądarki <pre>Chrome w wersji 49+</pre> lub <pre>Chrome for Android w wersji 57+</pre>Za wszelkie utrudnienia przepraszamy i czekamy z niecierpliwością na obsługę w innych przeglądarkach<br/><p style={{textAlign: 'right'}}>RAJZA Team</p></CardText>
+    </Card></Content>
+  }
+
+  return children
+}
